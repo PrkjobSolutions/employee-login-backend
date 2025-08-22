8d1482330913c99c5338ca7f93c253a059de8002
PRK <helpdesk.corporatehrsolutions@gmail.com>
2025-08-21 21:30:34 -0700

    Update cloudinary.js

---
+++ cloudinary.js
@@ -0,0 +1,10 @@
+// cloudinary.js
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: "dfb1yjqcc",
+  api_key: "625676797446458",
+  api_secret: "0sTGCjapNZknunk_b1prV7LMy40"
+});
+
+module.exports = cloudinary;
